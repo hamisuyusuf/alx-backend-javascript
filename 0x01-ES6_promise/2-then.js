823ace6da87dd2ee5cc6520300f19e82898a137b
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-07 16:00:39 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,3 @@
-// 2-then.js
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
@@ -13,4 +12,3 @@ export default function handleResponseFromAPI(promise) {
       return new Error();
     });
 }
-
