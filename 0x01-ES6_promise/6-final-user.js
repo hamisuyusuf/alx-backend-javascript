86bc63115a48de23e3f45dc1ab71afc6c6ed5a0b
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-07 18:01:13 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+export default handleProfileSignup;
 const handleProfileSignup = async (firstName, lastName, fileName) => {
   try {
     const [userPromise, photoPromise] = await Promise.allSettled([
@@ -17,5 +18,3 @@ const handleProfileSignup = async (firstName, lastName, fileName) => {
     return [{ status: 'rejected', value: error }];
   }
 };
-
-export default handleProfileSignup;
