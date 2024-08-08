d300037e7642d7c80ecbfa450035f5c78002370f
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-08 23:06:45 +0000

    update

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,12 +4,12 @@ export default class HolbertonClass {
       this._location = location;
     }
   
-    // The class is cast to a Number, return the size
+    // When class is cast to a Number, return the size
     valueOf() {
       return this._size;
     }
   
-    // The class is cast to a String, return the location
+    // When class is cast to a String, return the location
     toString() {
       return this._location;
     }
