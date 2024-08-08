a0a71501c0f2b7909b2649fc62726fd46df3c8dc
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-08 23:50:47 +0000

    Hot fix

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,4 @@
-export default class HolbertonClass {
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -33,13 +33,17 @@ export class StudentHolberton {
   }
 }
 
+// Create instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
+// Create instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+// Define and export listOfStudents
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
