e1eade6829caa3cbb8c3c964d309af2750be57ef
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-09 00:02:37 +0000

    Hot fix

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -4,14 +4,13 @@ export default class Car {
     this._motor = motor;
     this._color = color;
   }
-  
+
   cloneCar() {
     const Clone = this.constructor[Symbol.species] || this.constructor;
     return new Clone(this._brand, this._motor, this._color);
   }
-  
+
   static get [Symbol.species]() {
     return this;
   }
 }
-  
