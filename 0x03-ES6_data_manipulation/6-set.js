6bf58b6fd579ccd8c733ef0c491e015317a4de1c
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-13 23:48:26 +0000

    Tasks

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+function setFromArray(array) {
+    // Create and return a new Set from the array
+    return new Set(array);
+}
+
+// Example usage
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
