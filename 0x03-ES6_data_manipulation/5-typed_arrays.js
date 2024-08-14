751f4a676c6d97a2bde273c72187f59e7be33060
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-14 01:39:48 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,18 +1,18 @@
 export default function createInt8TypedArray(length, position, value) {
-    // Create a new ArrayBuffer of the given length
-    const buffer = new ArrayBuffer(length);
+  // Create a new ArrayBuffer of the given length
+  const buffer = new ArrayBuffer(length);
 
-    // Create an Int8Array view of the buffer
-    const int8Array = new Int8Array(buffer);
+  // Create an Int8Array view of the buffer
+  const int8Array = new Int8Array(buffer);
 
-    // Check if the position is within the valid range
-    if (position < 0 || position >= length) {
-        throw new Error('Position outside range');
-    }
+  // Check if the position is within the valid range
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    // Set the value of position
-    int8Array[position] = value;
+  // Set the value of position
+  int8Array[position] = value;
 
-    // Return  DataView of the buffer
-    return new DataView(buffer);
+  // Return  DataView of the buffer
+  return new DataView(buffer);
 }
