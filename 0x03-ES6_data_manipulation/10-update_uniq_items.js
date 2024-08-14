5cf57c05a2eccf6a0e8d735a842db32248af71be
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-14 01:53:38 +0100

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,19 +4,10 @@ export default function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
 
-    // Iterate over the Map and update the quantity for items with quantity 1
+  // Iterate over the Map and update the quantity for items with quantity 1
   map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
-        }
-    });
+    }
+  });
 }
-
-// Example usage:
-import groceriesList from "./9-groceries_list.js"; // Assuming this file exists
-
-const map = groceriesList();
-console.log(map);
-
-updateUniqueItems(map);
-console.log(map);
