d2cbbed052bfd920f78783fff1fd4d0c6a4234c0
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-14 01:27:12 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  // Return an empty string 
+  // Return an empty string
   if (startString === '') {
     return '';
   }
 
   // Filter and map set values
-    return [...set]
-        .filter((value) => value.startsWith(startString))
-        .map((value) => value.slice(startString.length))
-        .join('-');
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
