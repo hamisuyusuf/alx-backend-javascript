58729b80b9bcd1eabf28748fcc2f57a08e4a6d41
hamisu yusuf <111646226+hamisuyusuf@users.noreply.github.com>
2024-08-14 01:48:37 +0100

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 export default function updateUniqueItems(map) {
-    // Check if the argument is a Map
-    if (!(map instanceof Map)) {
-        throw new Error('Cannot process');
-    }
+  // Check if the argument is a Map
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
     // Iterate over the Map and update the quantity for items with quantity 1
-    map.forEach((value, key) => {
-        if (value === 1) {
-            map.set(key, 100);
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
         }
     });
 }
